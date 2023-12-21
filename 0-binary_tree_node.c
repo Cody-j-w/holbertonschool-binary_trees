87462a75bee22a604427d3bcf228be09fb4ede41
Cody <codywalenciak@gmail.com>
2023-12-21 11:27:10 -0600

    Fixed malloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,8 +7,9 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t *));
+    binary_tree_t *newnode = NULL;
 
+    newnode = malloc(sizeof(binary_tree_t));
     if (newnode == NULL)
         return (0);
 
