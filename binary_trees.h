b92ca8315baa23b07bfa49164aa8e77d2c7a9478
Cody <codywalenciak@gmail.com>
2023-12-21 13:07:45 -0600

    add binary_tree_insert_right

--- binary_trees.h
+++ binary_trees.h
@@ -21,5 +21,6 @@ typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *, int);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *, int);
 
 #endif
